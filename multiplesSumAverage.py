c7d987ef2818d41709e5a2e7d60700ab09786343
kwlee <keith.won.lee@gmail.com>
2017-09-05 14:15:09 -0700

    completed multiples sum average

---
+++ multiplesSumAverage.py
@@ -0,0 +1,19 @@
+# Multiples
+# Part 1
+for num in range(1,1000,2):
+    print num
+# Part 2
+for num in range(5,1000000,5):
+    print num
+# Sum List
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for num in a: 
+    sum = sum + num
+print sum 
+# Average List
+a = [1, 2, 5, 10, 255, 3]
+sum = 0
+for num in a: 
+    sum = sum + num
+print sum/len(a) 
